b234babff697b01a0e12b482c6af0afa8f2abfdb
lone wolf <lone_wolf_akela@icloud.com>
2021-06-02 16:56:53 +0800

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿
+namespace MicrosoftText2Speech
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxKey = new System.Windows.Forms.TextBox();
+            this.textBoxRegion = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonKeyRegion = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxRate = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.buttonText2Speech = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxText = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBoxStyle = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBoxVoice = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxLang = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxKey
+            // 
+            this.textBoxKey.Location = new System.Drawing.Point(112, 36);
+            this.textBoxKey.Name = "textBoxKey";
+            this.textBoxKey.Size = new System.Drawing.Size(478, 27);
+            this.textBoxKey.TabIndex = 0;
+            // 
+            // textBoxRegion
+            // 
+            this.textBoxRegion.Location = new System.Drawing.Point(112, 70);
+            this.textBoxRegion.Name = "textBoxRegion";
+            this.textBoxRegion.Size = new System.Drawing.Size(478, 27);
+            this.textBoxRegion.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Key";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Region";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonKeyRegion);
+            this.groupBox1.Controls.Add(this.textBoxKey);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBoxRegion);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(95, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 125);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "微软语音合成服务账号";
+            // 
+            // buttonKeyRegion
+            // 
+            this.buttonKeyRegion.Location = new System.Drawing.Point(638, 50);
+            this.buttonKeyRegion.Name = "buttonKeyRegion";
+            this.buttonKeyRegion.Size = new System.Drawing.Size(94, 40);
+            this.buttonKeyRegion.TabIndex = 4;
+            this.buttonKeyRegion.Text = "确定";
+            this.buttonKeyRegion.UseVisualStyleBackColor = true;
+            this.buttonKeyRegion.Click += new System.EventHandler(this.buttonKeyRegion_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Enabled = false;
+            this.buttonSave.Location = new System.Drawing.Point(554, 417);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(178, 42);
+            this.buttonSave.TabIndex = 5;
+            this.buttonSave.Text = "导出";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.textBoxRate);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.buttonPlay);
+            this.groupBox2.Controls.Add(this.labelStatus);
+            this.groupBox2.Controls.Add(this.buttonText2Speech);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.textBoxText);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.comboBoxStyle);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.comboBoxVoice);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.comboBoxLang);
+            this.groupBox2.Controls.Add(this.buttonSave);
+            this.groupBox2.Enabled = false;
+            this.groupBox2.Location = new System.Drawing.Point(95, 160);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(776, 528);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "语音合成";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(29, 99);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(703, 48);
+            this.textBox1.TabIndex = 19;
+            this.textBox1.Text = "要添加更多语音配置，请参阅 https://docs.microsoft.com/en-us/azure////cognitive-services/speech" +
+    "-service/language-support#neural-voices ，并相应地修改 voicedata.json 文件。";
+            // 
+            // textBoxRate
+            // 
+            this.textBoxRate.Location = new System.Drawing.Point(581, 64);
+            this.textBoxRate.Name = "textBoxRate";
+            this.textBoxRate.Size = new System.Drawing.Size(151, 27);
+            this.textBoxRate.TabIndex = 18;
+            this.textBoxRate.Text = "1";
+            this.textBoxRate.TextChanged += new System.EventHandler(this.textBoxRate_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(581, 33);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 20);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "语速";
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Enabled = false;
+            this.buttonPlay.Location = new System.Drawing.Point(279, 417);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(178, 42);
+            this.buttonPlay.TabIndex = 16;
+            this.buttonPlay.Text = "试听";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.Location = new System.Drawing.Point(29, 466);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(703, 57);
+            this.labelStatus.TabIndex = 15;
+            this.labelStatus.Text = "等待开始...";
+            // 
+            // buttonText2Speech
+            // 
+            this.buttonText2Speech.Enabled = false;
+            this.buttonText2Speech.Location = new System.Drawing.Point(29, 417);
+            this.buttonText2Speech.Name = "buttonText2Speech";
+            this.buttonText2Speech.Size = new System.Drawing.Size(178, 42);
+            this.buttonText2Speech.TabIndex = 14;
+            this.buttonText2Speech.Text = "合成";
+            this.buttonText2Speech.UseVisualStyleBackColor = true;
+            this.buttonText2Speech.Click += new System.EventHandler(this.buttonText2Speech_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 150);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(99, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "在此输入文本";
+            // 
+            // textBoxText
+            // 
+            this.textBoxText.Location = new System.Drawing.Point(29, 173);
+            this.textBoxText.Multiline = true;
+            this.textBoxText.Name = "textBoxText";
+            this.textBoxText.Size = new System.Drawing.Size(703, 227);
+            this.textBoxText.TabIndex = 12;
+            this.textBoxText.TextChanged += new System.EventHandler(this.textBoxText_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(408, 33);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "语气";
+            // 
+            // comboBoxStyle
+            // 
+            this.comboBoxStyle.FormattingEnabled = true;
+            this.comboBoxStyle.Location = new System.Drawing.Point(408, 64);
+            this.comboBoxStyle.Name = "comboBoxStyle";
+            this.comboBoxStyle.Size = new System.Drawing.Size(151, 28);
+            this.comboBoxStyle.TabIndex = 10;
+            this.comboBoxStyle.SelectedIndexChanged += new System.EventHandler(this.comboBoxStyle_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(218, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "语音";
+            // 
+            // comboBoxVoice
+            // 
+            this.comboBoxVoice.FormattingEnabled = true;
+            this.comboBoxVoice.Location = new System.Drawing.Point(218, 64);
+            this.comboBoxVoice.Name = "comboBoxVoice";
+            this.comboBoxVoice.Size = new System.Drawing.Size(151, 28);
+            this.comboBoxVoice.TabIndex = 8;
+            this.comboBoxVoice.SelectedIndexChanged += new System.EventHandler(this.comboBoxVoice_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "语言";
+            // 
+            // comboBoxLang
+            // 
+            this.comboBoxLang.FormattingEnabled = true;
+            this.comboBoxLang.Location = new System.Drawing.Point(29, 64);
+            this.comboBoxLang.Name = "comboBoxLang";
+            this.comboBoxLang.Size = new System.Drawing.Size(151, 28);
+            this.comboBoxLang.TabIndex = 6;
+            this.comboBoxLang.SelectedIndexChanged += new System.EventHandler(this.comboBoxLang_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(970, 693);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "MicrosoftText2Speech";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxKey;
+        private System.Windows.Forms.TextBox textBoxRegion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonKeyRegion;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox comboBoxLang;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBoxStyle;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBoxVoice;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxText;
+        private System.Windows.Forms.Button buttonText2Speech;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxRate;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+
+namespace MicrosoftText2Speech
+{
+    public partial class Form1 : Form
+    {
+        class KeyRegion
+        {
+            public string key { get; set; }
+            public string region { get; set; }
+        }
+        class Voice
+        {
+            public List<string> styles { get; set; } = new();
+        }
+        class VoiceLang
+        {
+            public Dictionary<string, Voice> voices { get; set; } = new();
+        }
+        class VoiceData
+        {
+            public Dictionary<string, VoiceLang> langs { get; set; } = new();
+        }
+        public class MediaPlayer
+        {
+            System.Media.SoundPlayer soundPlayer;
+            public MediaPlayer(byte[] buffer)
+            {
+                var memoryStream = new MemoryStream(buffer, true);
+                soundPlayer = new System.Media.SoundPlayer(memoryStream);
+            }
+            public void Play()
+            {
+                soundPlayer.Play();
+            }
+            public void Play(byte[] buffer)
+            {
+                soundPlayer.Stream.Seek(0, SeekOrigin.Begin);
+                soundPlayer.Stream.Write(buffer, 0, buffer.Length);
+                soundPlayer.Play();
+            }
+        }
+
+        KeyRegion keyregion;
+        VoiceData voicedata;
+        SpeechSynthesisResult audio;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if(File.Exists("key.json"))
+            {
+                keyregion = JsonSerializer.Deserialize<KeyRegion>(File.ReadAllText("key.json"));
+                textBoxKey.Text = keyregion.key;
+                textBoxRegion.Text = keyregion.region;
+            }
+            voicedata = JsonSerializer.Deserialize<VoiceData>(File.ReadAllText("voicedata.json"));
+            comboBoxLang.Items.Clear();
+            foreach(var lang in voicedata.langs.Keys)
+            {
+                comboBoxLang.Items.Add(lang);
+            }
+            comboBoxLang.SelectedIndex = 0;
+        }
+
+        private void buttonKeyRegion_Click(object sender, EventArgs e)
+        {
+            keyregion = new KeyRegion
+            {
+                key = textBoxKey.Text,
+                region = textBoxRegion.Text
+            };
+            string keyregion_json = JsonSerializer.Serialize(keyregion);
+            File.WriteAllText("key.json", keyregion_json);
+            groupBox2.Enabled = true;
+        }
+
+        private void comboBoxLang_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var lang = voicedata.langs[comboBoxLang.SelectedItem as string];
+            comboBoxVoice.Items.Clear();
+            foreach (var voice in lang.voices.Keys)
+            {
+                comboBoxVoice.Items.Add(voice);
+            }
+            comboBoxVoice.SelectedIndex = 0;
+            textChanged();
+        }
+
+        private void comboBoxVoice_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var voice = voicedata.langs[comboBoxLang.SelectedItem as string].voices
+                [comboBoxVoice.SelectedItem as string];
+            comboBoxStyle.Items.Clear();
+            foreach(var style in voice.styles)
+            {
+                comboBoxStyle.Items.Add(style);
+            }
+            comboBoxStyle.SelectedIndex = 0;
+            textChanged();
+        }
+
+        private async void buttonText2Speech_Click(object sender, EventArgs e)
+        {
+            buttonText2Speech.Enabled = false;
+            labelStatus.Text = "正在生成...";
+            var config = SpeechConfig.FromSubscription(keyregion.key, keyregion.region);
+            config.SetSpeechSynthesisOutputFormat(SpeechSynthesisOutputFormat.Riff24Khz16BitMonoPcm);
+            using var synthesizer = new SpeechSynthesizer(config, null);
+            string text = textBoxText.Text;
+            if(comboBoxStyle.SelectedItem as string != "General")
+            {
+                text =
+$@"<mstts:express-as style=""{comboBoxStyle.SelectedItem as string}"">
+{text}
+</mstts:express-as>";
+            }
+            text =
+$@"<speak 
+version=""1.0"" 
+xmlns=""https://www.w3.org/2001/10/synthesis"" 
+xmlns:mstts=""https://www.w3.org/2001/mstts""
+xml:lang=""{comboBoxLang.SelectedItem as string}"">
+<voice name=""{comboBoxVoice.SelectedItem as string}"">
+<prosody rate=""{textBoxRate.Text}"">
+{text}
+</prosody>
+</voice>
+</speak>";
+            audio = await synthesizer.SpeakSsmlAsync(text);
+            if (audio.Reason == ResultReason.Canceled)
+            {
+                var reason = SpeechSynthesisCancellationDetails.FromResult(audio);
+                labelStatus.Text = $"生成失败: {reason.ErrorDetails}";
+            }
+            else
+            {
+                labelStatus.Text = "生成完毕";
+                buttonSave.Enabled = true;
+                buttonPlay.Enabled = true;
+            }
+        }
+
+        void textChanged()
+        {
+            if (textBoxText.Text != "")
+            {
+                buttonText2Speech.Enabled = true;
+                buttonSave.Enabled = false;
+                buttonPlay.Enabled = false;
+            }
+        }
+
+        private void textBoxText_TextChanged(object sender, EventArgs e)
+        {
+            textChanged();
+        }
+
+        private async void buttonSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "wav文件|*.wav";
+            dialog.Title = "导出为wav文件";
+            dialog.ShowDialog();
+            if (dialog.FileName != "")
+            {
+                using var audiostream = AudioDataStream.FromResult(audio);
+                await audiostream.SaveToWaveFileAsync(dialog.FileName);
+                labelStatus.Text = "已保存";
+            }
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            MediaPlayer player = new(audio.AudioData);
+            player.Play();
+        }
+
+        private void comboBoxStyle_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textChanged();
+        }
+
+        private void textBoxRate_TextChanged(object sender, EventArgs e)
+        {
+            textChanged();
+        }
+    }
+}
